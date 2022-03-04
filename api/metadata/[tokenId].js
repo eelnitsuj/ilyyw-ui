6a417f661d78332959e9a1b5064c0a37c9a62dc0
Ryan Chang <rchang@videoamp.com>
2022-03-03 16:39:44 -0800

    add metadata endpoint

---
+++ api/metadata/[tokenId].js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { get } from 'lodash'
+
+export default async function handler (request, response) {
+  const metadata = get(
+    await axios.get('https://gateway.ipfs.io/ipfs/QmZZsnZDt5vcrcwGyzDd6wzLMBb357pPtZbxC6KvvFEbwY/hidden.json'),
+    'data'
+  )
+
+  // TODO: Maybe enumerate the hidden tokens?
+  // const result = {
+  //   ...metadata,
+  //   name: `${metadata.name} #${request.query.tokenId}`,
+  // }
+
+  response.status(200).json(metadata)
+}
