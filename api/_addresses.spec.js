aa143c5b5e79169e7aa70a2eae997b77dcaf9fbb
Ryan Chang <rchang@videoamp.com>
2022-03-08 10:11:14 -0800

    Addresses again (#12)
    * addresses
    
    * spec
    
    * more addys

--- api/_addresses.spec.js
+++ api/_addresses.spec.js
@@ -2,6 +2,7 @@ import { rawAddresses, cleanedAddresses } from './_addresses'
 
 describe('addresses', () => {
   it('has all unique addresses', () => {
+    console.log(`before: ${rawAddresses.length}, after: ${cleanedAddresses.length}`)
     expect(cleanedAddresses.length).toEqual(rawAddresses.length)
   })
 })
