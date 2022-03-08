1477140eff2f56b1000f772a0231d5cd6b276d4c
Ryan Chang <rchang@videoamp.com>
2022-03-08 12:57:36 -0800

    more cache stuff

--- api/metadata/[tokenId].js
+++ api/metadata/[tokenId].js
@@ -2,12 +2,13 @@ import axios from 'axios'
 import { get } from 'lodash'
 
 export default async function handler (request, response) {
-  const metadata = get(
-    await axios.get(
-      'https://gateway.ipfs.io/ipfs/QmUGJbt6SVy9Fd4uZ2ga3bhUCbUZfbk4kvnttYx13LX4Wi/hidden.json'
-    ),
-    'data'
-  )
+  // const ipfs =
+  //   'https://gateway.ipfs.io/ipfs/QmUGJbt6SVy9Fd4uZ2ga3bhUCbUZfbk4kvnttYx13LX4Wi/hidden.json'
+
+  const ipfs =
+    'https://ipfs.io/ipfs/QmUGJbt6SVy9Fd4uZ2ga3bhUCbUZfbk4kvnttYx13LX4Wi/hidden.json'
+
+  const metadata = get(await axios.get(ipfs), 'data')
 
   response
     .setHeader('Cache-Control', 'public, max-age=172800')
